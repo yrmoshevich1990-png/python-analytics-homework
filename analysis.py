a666d370fc5b708700097f24298ad864865393c6
oleksii <yrmoshevich1990@gmail.com>
2026-03-03 21:32:02 +0200

    temporary title change

--- analysis.py
+++ analysis.py
@@ -1,6 +1,6 @@
 mport pandas as pd
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 900, 500]}
 df = pd.DataFrame(data)
-print("Продажі по містах:")
+print("Продажі по містах (тимчасова версія):")
 print(df)
 print("Середнє значення:", df["sales"].mean())
